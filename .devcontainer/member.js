889ef20adb201f395624cfb9180b72e5f81f5302
aruiz69 <ruiz02israel@gmail.com>
2024-02-21 21:30:46 +0000

    Copilot second commit

---
+++ .devcontainer/member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    return {
+        "skills": [
+            {
+                "name": "C++",
+                "level": 5
+            },
+            {
+                "name": "Python",
+                "level": 4
+            },
+            {
+                "name": "Java",
+                "level": 3
+            }
+        ]
+    }
+}
